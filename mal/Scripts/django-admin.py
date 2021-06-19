2a6178302fee0dc7dd7e9688db9d36f6dcf7904b
kipkemoi dylan <kipkemoidylan254@gmail.com>
2021-06-19 13:08:29 -0700

    Disease Commit

---
+++ mal/Scripts/django-admin.py
@@ -0,0 +1,21 @@
+#!c:\users\dylan\desktop\disease-prediction-using-django-and-machine-learning-master\disease-prediction-using-django-and-machine-learning-master\mal\scripts\python.exe
+# When the django-admin.py deprecation ends, remove this script.
+import warnings
+
+from django.core import management
+
+try:
+    from django.utils.deprecation import RemovedInDjango40Warning
+except ImportError:
+    raise ImportError(
+        'django-admin.py was deprecated in Django 3.1 and removed in Django '
+        '4.0. Please manually remove this script from your virtual environment '
+        'and use django-admin instead.'
+    )
+
+if __name__ == "__main__":
+    warnings.warn(
+        'django-admin.py is deprecated in favor of django-admin.',
+        RemovedInDjango40Warning,
+    )
+    management.execute_from_command_line()
